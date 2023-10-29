08e46f4effa7deff8e39fe777698d594ba9b7567
K0L9 <kovalkola2@gmail.com>
2023-10-29 21:48:52 +0100

    Created alcohol calculation service

---
+++ Task3_IPA/AlcoholCalculationService.cs
@@ -0,0 +1,23 @@
+﻿namespace Task3_IPA
+{
+    public static class AlcoholCalculationService
+    {
+        public static double CalculateTotalValue(double value, int vesselCount)
+        {
+            if (vesselCount < 0 || value <= 0)
+                throw new ArgumentOutOfRangeException($"Argument can't be less than 0");
+
+            return value * vesselCount;
+        }
+        public static double CalculateAlcoCount(double value, int vesselCount, double alcoProcent)
+        {
+            if (alcoProcent < 0 && alcoProcent > 100) throw new ArgumentOutOfRangeException("Procent must be in 0 - 100");
+            try
+            {
+                double totalValue = CalculateTotalValue(value, vesselCount);
+                return totalValue / (alcoProcent * 100);
+            }
+            catch (Exception) { throw; }
+        }
+    }
+}
